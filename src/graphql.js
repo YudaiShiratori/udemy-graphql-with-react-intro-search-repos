d28367b61518e631b35f6f050f93637146dbd100
Yudai Shiratori <toritorikebabu1023x@gmail.com>
2020-06-12 00:04:48 +0900

    implement removeStar mutation

--- src/graphql.js
+++ src/graphql.js
@@ -38,6 +38,18 @@ export const ADD_STAR = gql`
     }
 `
 
+export const REMOVE_STAR = gql`
+    mutation removeStar ($input: RemoveStarInput!) {
+        removeStar (input: $input) {
+            starrable {
+                id
+                viewerHasStarred
+            }
+        }
+    }
+
+`
+
 export const ME = gql`
     query me {
         user(login: "iteachonudemy") {
